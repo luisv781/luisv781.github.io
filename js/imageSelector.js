5e76f2462d046f5da55bb8e1c58348a8cd3c0393
Luis <luis.villela021@gmail.com>
2025-05-27 20:33:56 -0500

    remove bunny 😔

--- js/imageSelector.js
+++ js/imageSelector.js
@@ -1,8 +1,6 @@
-const images = ["bunny.png", "sebsmirk.png", "tuffchris.png", "marco.png", "carloo.png", "german.png"];
+const images = ["sebsmirk.png", "tuffchris.png", "marco.png", "carloo.png", "german.png"];
 
-let randomNum = Math.floor(Math.random() * images.length);
+const randomNum = Math.floor(Math.random() * images.length);
 const chosenImage = images[randomNum];
-
-if (randomNum == 0) Math.floor(Math.random() * images.length);
 
 document.getElementById("cornerImg").setAttribute("src", "images/" + chosenImage);
